5ae0e6b7af379230913095a797afbe7b0861da11
omkarl28 <omkar.lawande28@gmail.com>
2023-06-23 09:13:43 +0530

    added model

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
 print('model')
+
+
+def model1():
+    print('adding code for model1')
+
+
+def model2():
+    print('adding code for model2')
+
+
+def model3():
+    print('adding code for model3')
